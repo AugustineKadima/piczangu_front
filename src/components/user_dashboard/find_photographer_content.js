6b39506fde0f0d652e175813d310a2091c217066
AugustineKadima <kadimasam@gmail.com>
2022-06-04 18:59:57 +0300

    Add photographer card

--- src/components/user_dashboard/find_photographer_content.js
+++ src/components/user_dashboard/find_photographer_content.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchUser from "./search_user";
+import PhotographerCardUser from "./photographer_card_user";
 
 export default function FindPhotographerContent(){
     return (
@@ -7,6 +8,9 @@ export default function FindPhotographerContent(){
             <div className="find_photographer_wrapper">
                 <div className="find_photographer_content_container">
                     <SearchUser />
+                    <div className="photographer_list_recyclerview">
+                        <PhotographerCardUser />
+                    </div>
 
                 </div>
             </div>
--- src/components/user_dashboard/photographer_card_user.js
+++ src/components/user_dashboard/photographer_card_user.js
@@ -4,8 +4,23 @@ export default function PhotographerCardUser(){
     return (
         <div>
             <div className="photographer_card_item">
-                
+                <div className="user_image_wrapper_card_item">
+
+                </div>
+                <div className="user_content_card_item">
+                    <h2>Luqman Photography</h2>
+                    <div className="location_wrapper">
+                        <div className="location_image_wrapper">
+
+                        </div>
+                         <h4>Location</h4>
+                    </div>
+                    <p>I am a photographer based in Nairobi. I shoot weddings, events and personal portraits. Book me today</p>
+
+                    <div className="ratings_container"></div>
+                </div>
             </div>
+   
         </div>
     )
 }
