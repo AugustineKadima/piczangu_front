05227ad06a6c25c03c5ed36fe8e5534b12bbf427
AugustineKadima <kadimasam@gmail.com>
2022-07-02 02:43:01 +0300

    Style user and photographer sidenav

--- src/components/photographer_dashboard/photographer_sidenav.js
+++ src/components/photographer_dashboard/photographer_sidenav.js
@@ -1,33 +1,35 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { BiLogOutCircle } from "react-icons/bi"
 
 export default function PhotographerSideNav(){
     return (
         <div>
         <div className="sidenav_user_container">
-            <div>
-                
-          
-                <div className="logo_container_top_nav logo_footer logo_dashboard">
-                    <NavLink to="/">
-                            <img  id="logo_img" alt="logo" src="../images/logo3.png"></img>
-                    </NavLink>
-                </div>
 
-                <div className="sidenav_select">
-                    <NavLink to="/photographer/home"><div className="side_nav_option active">Home</div> </NavLink>
-                    <NavLink to="/photographer/dashboard"><div className="side_nav_option active">dashboard</div> </NavLink>
-                    <NavLink to="/photographer/events"><div className="side_nav_option">Events</div></NavLink>
-                    <NavLink to="/photographer/earnings"><div className="side_nav_option active">Earnings</div> </NavLink>
-                    <NavLink to="/photographer/withdraw"> <div className="side_nav_option">Withdraw</div></NavLink>
+            <div className="side_nav_elements_container">
 
+                <div>
+                        <NavLink to="/">
+                                <img  id="logo_img" alt="logo" src="../images/logo3.png"></img>
+                        </NavLink>
+
+                    <div className="sidenav_select">
+                        <NavLink to="/photographer/home"><div className="side_nav_option active">Home</div> </NavLink>
+                        <NavLink to="/photographer/dashboard"><div className="side_nav_option active">dashboard</div> </NavLink>
+                        <NavLink to="/photographer/events"><div className="side_nav_option">Events</div></NavLink>
+                        <NavLink to="/photographer/earnings"><div className="side_nav_option active">Earnings</div> </NavLink>
+                        <NavLink to="/photographer/withdraw"> <div className="side_nav_option">Withdraw</div></NavLink>
+
+                    </div>
                 </div>
-            </div>
 
             
 
-            <div  className="logout">
-                <p>Logout</p>
+                <div  className="logout logout_2">
+                    <p><BiLogOutCircle/><span></span> Logout</p>
+                </div>
+
             </div>
         </div>
     </div>
--- src/components/side_nav_user.js
+++ src/components/side_nav_user.js
@@ -1,32 +1,34 @@
 import React from "react";
 import { NavLink } from "react-router-dom"
+import { BiLogOutCircle } from "react-icons/bi"
 
 
 export default function SideNavUser(){
     return (
         <div>
             <div className="sidenav_user_container">
-                <div>
-                    
-              
-                    <div className="logo_container_top_nav logo_footer logo_dashboard">
-                        <NavLink to="/">
-                                <img  id="logo_img" alt="logo" src="../images/logo3.png"></img>
-                        </NavLink>
-                    </div>
 
-                    <div className="sidenav_select">
-                        <NavLink to="/"><div className="side_nav_option active">Home</div> </NavLink>
-                        <NavLink to="/user/events"><div className="side_nav_option">Events</div></NavLink>
-                        <NavLink to="/photographers"> <div className="side_nav_option">Find Photographer</div></NavLink>
-
-                    </div>
-                </div>
-
+                <div className="side_nav_elements_container">
+                    <div>
+                        
                 
+                            <NavLink to="/">
+                                    <img  id="logo_img" alt="logo" src="../images/logo3.png"></img>
+                            </NavLink>
 
-                <div  className="logout">
-                    <p>Logout</p>
+                        <div className="sidenav_select">
+                            <NavLink to="/"><div className="side_nav_option active">Home</div> </NavLink>
+                            <NavLink to="/user/events"><div className="side_nav_option">Events</div></NavLink>
+                            <NavLink to="/photographers"> <div className="side_nav_option">Find Photographer</div></NavLink>
+
+                        </div>
+                    </div>
+
+                    
+
+                    <div  className="logout">
+                    <p><BiLogOutCircle/><span></span> Logout</p>
+                    </div>
                 </div>
             </div>
         </div>
